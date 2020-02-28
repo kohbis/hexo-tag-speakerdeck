dddfd0e1ceb8b87ab43a71a9247ebf006c73178d
Kohei Sugimoto <18735471+kohbis@users.noreply.github.com>
2020-02-28 20:53:48 +0900

    add keyword & refactoring

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 hexo.extend.tag.register('speakerdeck', function(args, content) {
   const id = args[0];
+  let slide = args[1] == null ? 1 : args[1];
 
-  let slide = "1";
-  if (args[1] !== null) slide = args[1];
-
-  return '<script async class="speakerdeck-embed" data-slide="' + slide + '" data-id="' + id + '" data-ratio="1.77777777777778" src="//speakerdeck.com/assets/embed.js"></script>';
+  return '\
+    <script async class="speakerdeck-embed"\
+      data-slide="' + slide + '"\
+      data-id="' + id + '"\
+      data-ratio="1.77777777777778"\
+      src="//speakerdeck.com/assets/embed.js">\
+    </script>';
 },{
   async: true
 });
